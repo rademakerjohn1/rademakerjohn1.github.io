1b47ba8d401fc1a90c7977501dc861cd00346102
John Rademaker <rademakerjohn1@gmail.com>
2020-07-13 22:50:45 -0400

    Fixed contact icon layout

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
     return(
         <SectionCard cardHeading={"Contact."} cardClass={"section-card contact"} 
         headingClass={"text-center"} section={"contact"}>
-            <div className="contact-icons">
+            <div className="contact-icons row">
                 <ContactIcon class="fab fa-linkedin-in" href="https://www.linkedin.com/in/rademakerjohn1/" />
                 <ContactIcon class="fas fa-envelope" href="mailto:rademakerjohn1@gmail.com" />
                 <ContactIcon class="fab fa-github" href="https://www.github.com/rademakerjohn1" />
--- src/components/ContactIcon/ContactIcon.js
+++ src/components/ContactIcon/ContactIcon.js
@@ -3,7 +3,7 @@ import "./ContactIcon.css";
 
 function ContactIcon(props) {
     return(
-    <a href={props.href} target="blank">
+    <a href={props.href} target="blank" className="col-4">
         <i className={props.class}></i>
     </a>
     )
