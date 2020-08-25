6bc041f7a2bbc3b830ef6ae62ae771817679a979
John Rademaker <rademakerjohn1@gmail.com>
2020-08-24 22:19:18 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ff3e2fc2f12041ac984afa4fe6f059a.js"
+  "/precache-manifest.96b5eef170b52bdf44b1ff2770504ca9.js"
 );
 
 self.addEventListener('message', (event) => {
