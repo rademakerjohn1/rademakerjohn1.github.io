ea50989d132d2445f731eaa7195db0ea523270ce
John Rademaker <rademakerjohn1@gmail.com>
2020-09-03 12:44:36 -0400

    added description of tools used to portfolio projects

--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -9,10 +9,13 @@ function GridItem(props) {
             <div className="description">
                 <h4>{props.title}</h4>
                 <p>{props.description}</p>
+                <hr />
+                <p>{props.tools}</p>
                 <div className="project-links">
-                <a target="blank" href={props.repo}>Repo</a>
+                <a target="blank" href={props.repo}>Repository</a>
                 <a target="blank" href={props.demo}>Deployed</a>
                 </div>
+
             </div>
             }
             <img alt="" src={require(`${props.img}`)} width="100%" className="card-img-top img-responsive" />
